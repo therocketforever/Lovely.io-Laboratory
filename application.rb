400823ee6ac1b5d95c5352cfbd5cae98b5f9866d
DON who is a ghost <therocketforever@me.com>
2012-08-28 15:29:04 -0400

    Preparation for AJAXification

--- application.rb
+++ application.rb
@@ -18,18 +18,18 @@ class Location
   field :state
   field :postal_code
   
-  # Location::address=() sets the value of self.addressable & passes this value to self.geocode as the encodeing argument
+# Location::address=() sets the value of self.addressable & passes this value to self.geocode as the encodeing argument
   def address=(query)
     self.addressable = query unless self.addressable
     self.geocode
   end
   
-  # Location::address returns the value of self.addressable
+# Location::address returns the value of self.addressable
   def address
     self.addressable
   end
   
-  # Location::geocode() querys the Geocoder::search method, sets the value of location to the returned result & sets the various properties of the particular instance of Location to the corrisponding values. location.address is set to location.addressable to preserve the namespace for Location::address=() & Location::address.
+# Location::geocode() querys the Geocoder::search method, sets the value of location to the returned result & sets the various properties of the particular instance of Location to the corrisponding values. location.address is set to location.addressable to preserve the namespace for Location::address=() & Location::address.
   def geocode( query = self.addressable )
     location = Geocoder.search( query ).pop
     self.coordinates = location.coordinates
@@ -41,6 +41,8 @@ class Location
 end
 
 get '/script.js' do
+  content_type "text/javascript", :charset => 'utf-8'
+  coffee :script
 end
 
 get '/' do
@@ -50,7 +52,14 @@ end
 __END__
 
 @@layout
-= yield
+!!! 5
+%html
+  %head
+    -#%script{src="http://cdn.lovely.io/core.js"}
+    
+    %script{src: "/script.js"}
+%body
+  = yield
 
 @@index
 %p Hello World
